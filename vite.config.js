3c8d600c11924e275db69758e253b89a827e6227
Joe Mac <jhmccarney@gmail.com>
2024-05-28 08:34:15 +0100

    updated vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: 'src/assets/**',
-  base: '/gallery-models',
+  base: '/gallery-models/',
   plugins: [react()],
 })
