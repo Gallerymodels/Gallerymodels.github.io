6581f28b8052f064a65b0bc184d5a0342328aa9a
Joe Mac <jhmccarney@gmail.com>
2024-05-28 09:56:24 +0100

    further fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: 'src/assets/**',
-  base: process.env.NODE_ENV === 'production' ? '/gallery-models/' : '/',
   plugins: [react()],
 })
