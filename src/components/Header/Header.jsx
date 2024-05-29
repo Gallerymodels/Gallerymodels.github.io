8986e121af51f465f21d27e8e8847176ce966c5e
Joe Mac <jhmccarney@gmail.com>
2024-05-29 12:45:09 +0100

    modified opengraph meta tags

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import Nav from './Nav'
 
 const Header = () => {
   return (
-    <div className='w-full h-10 md:h-20 p-4 sticky top-0 z-20 mx-auto flex flex-wrap items-center justify-between bg-gradient-to-b from-white from-60%'>
+    <h1 className='w-full h-10 md:h-20 p-4 sticky top-0 z-20 mx-auto flex flex-wrap items-center justify-between bg-gradient-to-b from-white from-60%'>
         <Link to="/" className="font-dejaRegular text-gray text-xl">
                 Gallery Models
         </Link>
         <Nav />
-    </div>
+    </h1>
   )
 }
 
